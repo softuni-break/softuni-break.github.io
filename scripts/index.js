61d9b10855e9cae76d5a20c77a1c48397b233110
Viktor Kostadinov <viktor.pts@abv.bg>
2020-09-16 10:57:27 +0300

    Added initial time setup

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,15 @@ import { timeHandlers } from './time.js';
 import { elements } from './elements.js';
 import { partners } from './partners.js';
 import { slider } from './slider.js';
+import { parseQueryString } from './util.js';
 
 document.onload = (() => {
+    if (location.search !== null) {
+        const { m, s } = parseQueryString(location.search);
+        elements.time.minutes().textContent = m || '00';
+        elements.time.seconds().textContent = s || '00';
+        buttonHandlers.start();
+    }
 
     elements.time.time().addEventListener('mousemove', buttonHandlers.pause);
     elements.time.time().addEventListener('mouseleave', buttonHandlers.start);
