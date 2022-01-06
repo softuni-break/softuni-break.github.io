fba79171b577c4a2e33b6edb6ae29b03bc81c92e
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2022-01-06 10:52:13 +0200

    Update partners.js

--- scripts/partners.js
+++ scripts/partners.js
@@ -2,11 +2,12 @@ export const partners = [
     { src: "./public/partners/coca-cola.png", name: "Coca Cola HBC Bulgaria" },
     { src: "./public/partners/draft-kings.png", name: "DraftKings" },
     { src: "./public/partners/indeaver.png", name: "Indeavr" },
-    { src: "./public/partners/infragistics.png", name: "Infragistics" },
     { src: "./public/partners/motion-software.jpg", name: "Motion Software" },
     { src: "./public/partners/post-bank.png", name: "PostBank" },
     { src: "./public/partners/smart-it.png", name: "SmartIt" },
     { src: "./public/partners/software-group.png", name: "Software Group" },
     { src: "./public/partners/super-hosting.png", name: "SuperHosting" },
     { src: "./public/partners/xs-software.png", name: "XS Software" },
+    { src: "./public/partners/taulia.png", name: "Taulia" },
+    { src: "./public/partners/createx.png", name: "CreateX" }
 ];
