7d3ffad16eb216441d9525c1569d2b11077e49d0
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2022-09-09 15:04:42 +0300

    Update partners.js
    update partners

--- scripts/partners.js
+++ scripts/partners.js
@@ -3,12 +3,13 @@ export const partners = [
     { src: "./public/partners/draft-kings.png", name: "DraftKings" },
     { src: "./public/partners/indeaver.png", name: "Indeavr" },
     { src: "./public/partners/post-bank.png", name: "PostBank" },
-    { src: "./public/partners/smart-it.png", name: "SmartIt" },
+    { src: "./public/partners/smartit.png", name: "SmartIt" },
     { src: "./public/partners/software-group.png", name: "Software Group" },
     { src: "./public/partners/super-hosting.png", name: "SuperHosting" },
     { src: "./public/partners/taulia.png", name: "Taulia" },
     { src: "./public/partners/createx.png", name: "CreateX" },
-     { src: "./public/partners/pharVision.png", name: "PharVision" },
-     { src: "./public/partners/schwarz.png", name: "SchwarzIT" },
-    { src: "./public/partners/pokerstars.png", name: "Pokerstars" }
+    { src: "./public/partners/pharVision.png", name: "PharVision" },
+    { src: "./public/partners/schwarz.png", name: "SchwarzIT" },
+    { src: "./public/partners/pokerstars.png", name: "Pokerstars" },
+    { src: "./public/partners/bosch.png", name: "Bosch.IO" }
 ];
