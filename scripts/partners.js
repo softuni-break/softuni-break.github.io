20d9210c954a442058c2364f37a9c3fabe96fdd0
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2023-08-22 21:02:38 +0300

    Update partners.js

--- scripts/partners.js
+++ scripts/partners.js
@@ -1,15 +1,16 @@
 export const partners = [
+    { src: "./public/partners/ambitioned.png", name: "Ambitioned" },
+    { src: "./public/partners/bosch.png", name: "Bosch.IO" },
     { src: "./public/partners/coca-cola.png", name: "Coca Cola HBC" },
-    { src: "./public/partners/draft-kings.png", name: "DraftKings" },
-    { src: "./public/partners/indeaver.png", name: "Indeavr" },
-    { src: "./public/partners/post-bank.png", name: "PostBank" },
-    { src: "./public/partners/smrtit.png", name: "SmartIt" },
-    { src: "./public/partners/software-group.png", name: "Software Group" },
-    { src: "./public/partners/super-hosting.png", name: "SuperHosting" },
-    { src: "./public/partners/taulia.png", name: "Taulia" },
     { src: "./public/partners/createx.png", name: "CreateX" },
-    { src: "./public/partners/pharVision.png", name: "PharVision" },
-    { src: "./public/partners/schwarz.png", name: "SchwarzIT" },
-    { src: "./public/partners/pokerstars.png", name: "Pokerstars" },
-    { src: "./public/partners/bosch.png", name: "Bosch.IO" }
+    { src: "./public/partners/draft-kings.png", name: "Draft Kings" },
+    { src: "./public/partners/dxc.png", name: "DXC Technology" },
+    { src: "./public/partners/flutter.svg", name: "Flutter" },
+    { src: "./public/partners/taulia.png", name: "Taulia" },
+    { src: "./public/partners/indeavr.png", name: "Indeavr" },
+    { src: "./public/partners/phar-vision.png", name: "Phar Vision" },
+    { src: "./public/partners/postbank.png", name: "Postbank" },
+    { src: "./public/partners/smart-it.png", name: "SmartIT" },
+    { src: "./public/partners/software-group.png", name: "Software Group" },
+    { src: "./public/partners/superhosting.png", name: "SuperHosting" }
 ];
